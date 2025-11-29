9ac1048a1ac9b84b41baf5edf52b565bd2f80dbe
Ahliman Shirinov <ahliman@example.com>
2025-11-29 18:53:08 +0300

    Vehicle class

--- src/parking/smart/assigment/model/Vehicle.java
+++ src/parking/smart/assigment/model/Vehicle.java
@@ -1,5 +1,36 @@
 package parking.smart.assigment.model;
 
+import java.time.LocalDateTime;
+
 public class Vehicle {
+    private String plate;
+    private String size;
+    private boolean isParked;
+    private String assignedSpotID;
+    private LocalDateTime entryTime;
+
+    public Vehicle(String plate, String size) {
+        this.plate = plate;
+        this.size = size;
+        this.isParked = false;
+    }
+
+    public void Park() {
+        this.isParked = true;
+        this.entryTime = LocalDateTime.now();
+    }
+
+    public void leave() {
+        this.isParked = false;
+    }
+
+    public void assignSpot(String spotID) {
+        this.assignedSpotID = spotID;
+    }
+
+    @Override
+    public String toString() {
+        return plate + " | " + size + " | parked=" + isParked + " | spot=" + assignedSpotID;
+    }
 
 }
